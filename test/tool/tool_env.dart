1f4510e94c1d1725f254dd8b754cf45dfba39134
Romulo Carvalho <oakromulo@gmail.com>
2019-10-15 22:34:51 -0300

    fix/tool-env-style

--- test/tool/tool_env.dart
+++ test/tool/tool_env.dart
@@ -4,10 +4,10 @@ Future<void> main() async {
   final configUrl = Platform.environment['APP_CONFIG_URL'];
 
   File('lib/.env.dart').writeAsString(configUrl.isNotEmpty
-    ? '''
+      ? '''
 /// @nodoc
 const configUrl = '$configUrl';\n'''
-    : '''
+      : '''
 /// @nodoc
 const String configUrl = null;\n''');
 }
