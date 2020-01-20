3da7a74d0528c0077b0b6f687070c76c0f4d5c0f
Romulo Carvalho <oakromulo@gmail.com>
2020-01-20 13:34:01 +0200

    feature/recase-utils

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -4,6 +4,7 @@ library util;
 import 'dart:async' show StreamController, StreamSubscription;
 
 import 'package:flutter/foundation.dart' show debugPrint;
+import 'package:recase/recase.dart' show ReCase;
 
 /// @nodoc
 String dartEnv() =>
@@ -18,6 +19,27 @@ void debugError(dynamic e, [dynamic s]) => _debug(() {
 void debugLog(dynamic msg) => _debug(() {
       debugPrint('flutter_analytics ➲ ${msg.toString()} @ ${_isoNow()}\n');
     });
+
+/// @nodoc
+String camelCase(String s) => _toCase(s, (s) => ReCase(s).camelCase);
+
+/// @nodoc
+String paramCase(String s) => _toCase(s, (s) => ReCase(s).paramCase);
+
+/// @nodoc
+String titleCase(String s) => _toCase(s, (s) => ReCase(s).titleCase);
+
+String _toCase(String string, String Function(String) recaseFunction) {
+  try {
+    if (string == null || string.isEmpty) {
+      throw null;
+    }
+
+    return recaseFunction(string);
+  } catch (_) {
+    return null;
+  }
+}
 
 void _debug(void Function() exec) {
   if (!bool.fromEnvironment('dart.vm.product')) {
