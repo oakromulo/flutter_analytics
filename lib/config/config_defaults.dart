fa946427400a384ec2b1d71773b738275cd2964a
Romulo Carvalho <oakromulo@gmail.com>
2020-02-13 18:57:10 +0200

    feature/refresh-interval-config

--- lib/config/config_defaults.dart
+++ lib/config/config_defaults.dart
@@ -6,6 +6,7 @@ const defaults = <String, dynamic>{
   'defaultTimeoutSecs': 60,
   'flushAtLength': 100,
   'flushAtSecs': 300,
+  'locationRefreshIntervalSecs': 30,
   'maxQueueLength': 10000,
   'sessionTimeoutSecs': 1800
 };
