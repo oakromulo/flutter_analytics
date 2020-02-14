6b8a868019ff42f47802087e323c5f352760c7b6
Romulo Carvalho <oakromulo@gmail.com>
2020-02-14 17:12:47 +0200

    feature/reset-session-on-backgrounded
    - reset sessionId when app lifecycle state changes to paused
    - minor refactor of `store`

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -12,6 +12,7 @@ import 'package:uuid/uuid.dart' show Uuid;
 import '../config/config.dart' show Config;
 import '../debug/debug.dart' show Debug;
 import '../event/event.dart' show Event, EventBuffer;
+import '../lifecycle/lifecycle.dart' show AppLifecycle, AppLifecycleState;
 
 /// @nodoc
 class Store {
@@ -19,20 +20,17 @@ class Store {
   factory Store() => _store;
 
   Store._internal() {
-    _hasInit = _init();
     _buffer = EventBuffer()..enqueue(Event(_onSetup)).catchError(Debug().error);
   }
 
   static final _store = Store._internal();
 
-  LocalStorage _storage;
   EventBuffer _buffer;
+  LocalStorage _storage;
 
   String _groupId;
   String _path;
   String _userId;
-
-  Future _hasInit;
 
   /// @nodoc
   Future<String> get anonymousId async =>
@@ -58,7 +56,7 @@ class Store {
 
   /// @nodoc
   Future<String> get sessionId async {
-    if (await _isSessionInvalid()) {
+    if (await _isSessionExpired()) {
       await _resetSession();
     }
 
@@ -89,8 +87,33 @@ class Store {
     }
   }
 
-  Future<void> _set(String key, Future<String> val) =>
-      _buffer.enqueue(Event(() => _onSet(key, val))).catchError(Debug().error);
+  Future _initFiles() async {
+    try {
+      _path = (await getApplicationDocumentsDirectory()).path;
+
+      _groupId ??= await File('$_path/group_id').readAsString();
+      _userId ??= await File('$_path/user_id').readAsString();
+    } catch (_) {
+      // do nothing
+    }
+  }
+
+  Future<bool> _isSessionExpired() async {
+    final t0 = DateTime.tryParse(await _get('sessionStart') ?? '');
+
+    if (t0 == null) {
+      return true;
+    }
+
+    final sessionTimeout = Config().sessionTimeout;
+    return DateTime.now().toUtc().isAfter(t0.add(sessionTimeout));
+  }
+
+  void _onAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      _resetSession().catchError(Debug().error);
+    }
+  }
 
   Future<String> _onGet(String key) async {
     String value;
@@ -109,39 +132,12 @@ class Store {
       val.then((v) => _storage.setItem(key, {'v': v}));
 
   Future<void> _onSetup() async {
-    await _hasInit;
+    await _initFiles();
 
     _storage = LocalStorage('__analytics_storage__');
     await _storage.ready;
-  }
 
-  Future _init() async {
-    try {
-      _path = (await getApplicationDocumentsDirectory()).path;
-
-      _groupId ??= await File('$_path/group_id').readAsString();
-      _userId ??= await File('$_path/user_id').readAsString();
-    } catch (_) {
-      // do nothing
-    }
-  }
-
-  Future<bool> _isSessionInvalid() async {
-    final t0 = DateTime.tryParse(await _get('sessionStart') ?? '');
-
-    if (t0 == null) {
-      return true;
-    }
-
-    final sessionTimeout = Config().sessionTimeout;
-    return DateTime.now().toUtc().isAfter(t0.add(sessionTimeout));
-  }
-
-  Future<void> _resetSession() async {
-    final sessionStart = DateTime.now().toUtc().toIso8601String();
-
-    await _set('sessionStart', Future.value(sessionStart));
-    await _set('sessionId', Future.value(Uuid().v4()));
+    AppLifecycle().subscribe(_onAppLifecycleState);
   }
 
   Future<String> _resetAnonymousId() async {
@@ -151,6 +147,16 @@ class Store {
 
     return id;
   }
+
+  Future<void> _resetSession() async {
+    final sessionStart = DateTime.now().toUtc().toIso8601String();
+
+    await _set('sessionStart', Future.value(sessionStart));
+    await _set('sessionId', Future.value(Uuid().v4()));
+  }
+
+  Future<void> _set(String key, Future<String> val) =>
+      _buffer.enqueue(Event(() => _onSet(key, val))).catchError(Debug().error);
 
   Future<String> _udid() async {
     try {
